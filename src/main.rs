28386cc280c746d9c63211ee165bd0ef83e78325
Mike Cluck <mcluck90@gmail.com>
2018-09-11 07:47:08 -0600

    Displays output of parsing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 mod dom;
+mod parser;
 
 use dom::*;
+use parser::parse;
 use std::collections::HashMap;
 
 fn main() {
+	println!("{}", "Text Element:");
 	let just_text = text("Testing text element".into());
-	println!("{:?}", just_text);
+	println!("{:?}\n", just_text);
 
+	println!("{}", "Empty Element:");
 	let empty_el = elem("empty".into(), HashMap::new(), vec![]);
-	println!("{:?}", empty_el);
+	println!("{:?}\n", empty_el);
 
 	let mut attrs = HashMap::new();
 	attrs.insert("id".to_string(), "test".to_string());
 	let root = elem("h1".into(), attrs, vec![text("Hello world!".into())]);
-	println!("{:?}", root);
+
+	println!("{}", "Element with One Child:");
+	println!("{:?}\n", root);
+
+	let input = "
+	<html>
+		<body>
+			<h1>Hello, world!</h1>
+		</body>
+	</html>";
+	let output = parse(input.into());
+	println!("{}", "Complex DOM:");
+	println!("{: >2?}", output);
 }
